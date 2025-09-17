29b1997a16d6a155ac0e5ef54b0ffeb2f0a6e7e3
sushitoad <36284902+sushitoad@users.noreply.github.com>
2025-09-17 09:48:30 -0700

    starting refactor

--- scripts/tower_manager.gd
+++ scripts/tower_manager.gd
@@ -1,8 +1,25 @@
 extends Node2D
 
 @export var protoTower: PackedScene
-#buttz
-#also refactor so build ui is telling this which tower to place
+
+#new refactor is combining these scripts, here are some variables from other script
+var placingingTower: bool = false
+var isMouseOverButton: bool = false
+var mousePosition: Vector2
+
+func _process(delta: float) -> void:
+	mousePosition = get_global_mouse_position()
+	#if ghostTower != null:
+		#ghostTower.position = mousePosition
+	if Input.is_action_just_pressed("Multi Action") and isMouseOverButton == false:
+		if placingingTower:
+			#PlaceTower(mousePosition)
+			pass
+
+func SpawnTower():
+	print("spawning new tower to place")
+	placingingTower = true
+	#ghostTower.visible = true
 
 func PlaceNewTower():
 	var newTower = protoTower.instantiate()
