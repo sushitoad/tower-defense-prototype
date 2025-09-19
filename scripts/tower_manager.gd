176ec93cdccf6bd8cfb668f72823667bbcf61fbc
sushitoad <36284902+sushitoad@users.noreply.github.com>
2025-09-19 16:16:17 -0700

    refactoring in progress
    doing well

--- scripts/tower_manager.gd
+++ scripts/tower_manager.gd
@@ -1,29 +1,36 @@
 extends Node2D
 
 @export var protoTower: PackedScene
+@export var buildUI: Control
+
+var ghostProtoTower: Sprite2D
 
 #new refactor is combining these scripts, here are some variables from other script
-var placingingTower: bool = false
+var placingTower: bool = false
 var isMouseOverButton: bool = false
 var mousePosition: Vector2
 
+func _ready() -> void:
+	ghostProtoTower = buildUI.find_child("GhostProtoTower")
+	#this needs to connect the button pressed signal to SpawnTower
+
 func _process(delta: float) -> void:
 	mousePosition = get_global_mouse_position()
-	#if ghostTower != null:
-		#ghostTower.position = mousePosition
-	if Input.is_action_just_pressed("Multi Action") and isMouseOverButton == false:
-		if placingingTower:
+	if ghostProtoTower != null:
+		ghostProtoTower.position = mousePosition
+	if Input.is_action_just_pressed("Multi Action") and buildUI.isMouseOverButton == false:
+		if placingTower:
 			#PlaceTower(mousePosition)
 			pass
 
 func SpawnTower():
 	print("spawning new tower to place")
-	placingingTower = true
-	#ghostTower.visible = true
+	placingTower = true
+	ghostProtoTower.visible = true
 
-func PlaceNewTower():
+func PlaceSpawnedTower():
 	var newTower = protoTower.instantiate()
 	add_child(newTower)
-	newTower.position = get_global_mouse_position()
+	newTower.position = mousePosition
 
 	
