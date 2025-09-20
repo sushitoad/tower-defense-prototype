d307d763ac368b9e89eeae4656ce905196f20bd9
quillenink <36284902+sushitoad@users.noreply.github.com>
2025-09-19 18:49:26 -0700

    refactor finished

--- scripts/build_ui.gd
+++ scripts/build_ui.gd
@@ -1,24 +1,9 @@
 extends Control
 
-#@export var towerManager: Node2D
-#dont do it this way reference the proto tower in tower manager instead
+
 var isMouseOverButton: bool = false
 
 #use enums and match to feed tower type into other scripts
-
-func _process(delta: float) -> void:
-	pass
-
-#func BuildTower():
-	#print("build le towr")
-	#buildingTower = true
-	#ghostTower.visible = true
-#
-#func PlaceTower(position: Vector2):
-	#print(mousePosition)
-	#ghostTower.visible = false
-	#towerManager.PlaceNewTower()
-	#buildingTower = false
 
 func _on_button_mouse_entered() -> void:
 	isMouseOverButton = true
--- scripts/tower_manager.gd
+++ scripts/tower_manager.gd
@@ -5,32 +5,30 @@ extends Node2D
 
 var ghostProtoTower: Sprite2D
 
-#new refactor is combining these scripts, here are some variables from other script
 var placingTower: bool = false
-var isMouseOverButton: bool = false
 var mousePosition: Vector2
 
 func _ready() -> void:
 	ghostProtoTower = buildUI.find_child("GhostProtoTower")
-	#this needs to connect the button pressed signal to SpawnTower
+	var buildButton = buildUI.find_child("Button")
+	#I will need a different solution when I have more tower options and more buttons
+	buildButton.pressed.connect(SpawnTower)
 
 func _process(delta: float) -> void:
 	mousePosition = get_global_mouse_position()
 	if ghostProtoTower != null:
 		ghostProtoTower.position = mousePosition
-	if Input.is_action_just_pressed("Multi Action") and buildUI.isMouseOverButton == false:
+	if Input.is_action_just_pressed("LeftClick") and buildUI.isMouseOverButton == false:
 		if placingTower:
-			#PlaceTower(mousePosition)
-			pass
+			PlaceSpawnedTower()
 
 func SpawnTower():
-	print("spawning new tower to place")
 	placingTower = true
 	ghostProtoTower.visible = true
 
 func PlaceSpawnedTower():
+	ghostProtoTower.visible = false
 	var newTower = protoTower.instantiate()
 	add_child(newTower)
 	newTower.position = mousePosition
-
-	
+	placingTower = false
