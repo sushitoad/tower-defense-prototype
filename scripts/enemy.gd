3e1f6145d0b2a9bef212dce9f1be4841bf2bab10
sushitoad <36284902+sushitoad@users.noreply.github.com>
2025-10-12 20:21:40 -0700

    enemy has FindTarget

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,17 +1,32 @@
 extends CharacterBody2D
 
-@export var moveToPoint: Vector2
+var moveToPoint: Vector2
 @export var moveSpeed: float = 10
+@export var range: float = 20
+
+func _ready() -> void:
+	FindTarget()
 
 func _physics_process(delta: float) -> void:
-	
-	if position.distance_to(moveToPoint) > 4:
+	if position.distance_to(moveToPoint) > range:
 		velocity = position.direction_to(moveToPoint) * moveSpeed
+	else:
+		velocity = Vector2.ZERO
 	
 	move_and_slide()
 	
 func _process(delta: float) -> void:
 	if velocity.x > 0:
 		$Sprite2D.flip_h = true
-	else:
+	elif velocity.x < 0:
 		$Sprite2D.flip_h = false
+		
+func FindTarget():
+	print("finding target")
+	var closestTarget: Vector2 = Vector2(10000000000, 10000000000)
+	for target in get_tree().get_nodes_in_group("target"):
+		if target.position.distance_to(position) < closestTarget.distance_to(position):
+			closestTarget = target.position
+	moveToPoint = closestTarget
+	
+	
