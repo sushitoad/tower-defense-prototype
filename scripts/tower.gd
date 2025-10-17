8a94f60217c2e86060301bda4f6c6e7f39ad2093
quillenink <36284902+sushitoad@users.noreply.github.com>
2025-10-17 13:23:52 -0700

    almost got dormant towers

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -2,6 +2,8 @@ extends StaticBody2D
 
 @export var maxHealth: int = 100
 @export var allure: float = 10
+@export var dormantTime: float = 10
+@export var dormantColor: Color = Color(1, 1, 1, 0.5)
 var currentHealth: int
 var isDestroyed: bool = false
 
@@ -10,6 +12,7 @@ signal on_destroyed
 func _ready() -> void:
 	currentHealth = maxHealth
 	isDestroyed = false
+	$DormantTimer.timeout.connect(WakeThisTower)
 	
 func TakeDamage(damage: int):
 	currentHealth -= damage
@@ -17,4 +20,10 @@ func TakeDamage(damage: int):
 	if currentHealth <= 0:
 		isDestroyed = true
 		on_destroyed.emit()
-		call_deferred("queue_free")
+		$DormantTimer.start(dormantTime)
+		$Sprite2D.modulate = dormantColor
+
+func WakeThisTower():
+	isDestroyed = false
+	currentHealth = maxHealth
+	$Sprite2D.modulate = Color(1, 1, 1, 1)
