18d8c7b8c03ddd84f6ec02c9489cac47f1f00484
quillenink <36284902+sushitoad@users.noreply.github.com>
2025-10-22 11:05:28 -0700

    towers target enemies now!

--- scripts/basic_tower.gd
+++ scripts/basic_tower.gd
@@ -7,28 +7,29 @@ extends Area2D
 @export var attackSpeed: float = 1
 @export var attackDamage: int = 10
 var attackRange: float
+var enemiesInRange = Array()
 var currentTarget: CharacterBody2D
 var tower: StaticBody2D
 
 func _ready() -> void:
 	var circleShape: Shape2D = $CollisionShape2D.shape
-	attackRange = circleShape.radius / 100
+	attackRange = circleShape.radius
 	tower = get_parent()
 
 func _process(delta: float) -> void:
 	if currentTarget == null:
-		var enemies = get_tree().get_nodes_in_group("enemy")
-		for enemy in enemies:
-			if tower.position.distance_to(enemy.position) > attackRange:
-				#i think this thinning the list approach is just not gonna work
-				#for some reason it keeps one of the enemies in the array even when it should be out of range
-				#so its either the array logic, or the distance logic, and I did a solid cursory check of distance
-				enemies.remove_at(enemies.find(enemy))
-		var closestEnemy: CharacterBody2D
-		print(enemies.size())
-
-	#find the closest one and set it to current target
+		if !enemiesInRange.is_empty():
+			var closestEnemy = enemiesInRange[0]
+			for enemy in enemiesInRange:
+				if position.distance_to(enemy.position) < position.distance_to(closestEnemy.position):
+					closestEnemy = enemy
+			currentTarget = closestEnemy
+	print(currentTarget)
 
 func _on_body_exited(body: Node2D) -> void:
 	if currentTarget == body:
 		currentTarget = null
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		enemiesInRange.append(body)
