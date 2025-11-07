1482150fb1566c09caa2a51ce8103cf40f860c5b
sushitoad <36284902+sushitoad@users.noreply.github.com>
2025-11-07 11:49:42 -0800

    starting on charge icons
    does not work, not showing up

--- scripts/build_ui.gd
+++ scripts/build_ui.gd
@@ -1,9 +1,11 @@
 extends Control
 
 var isMouseOverButton: bool = false
+@export var chargeIcons: Array[TextureRect]
 
 func _ready() -> void:
 	ToggleTowerButtons()
+	_on_charge_bar_charge_amount_changed()
 
 func _on_button_mouse_entered() -> void:
 	isMouseOverButton = true
@@ -18,3 +20,11 @@ func ToggleTowerButtons():
 	else:
 		$CanvasLayer/BasicTowerButton.visible = true
 		$CanvasLayer/ChargeTowerButton.visible = true
+
+func _on_charge_bar_charge_amount_changed() -> void:
+	for iconIndex in chargeIcons.size():
+		if ($CanvasLayer/ChargeBar.numberOfCharges - 1) >= iconIndex:
+			chargeIcons[iconIndex].visible = true
+		else:
+			chargeIcons[iconIndex].visible = false
+		
