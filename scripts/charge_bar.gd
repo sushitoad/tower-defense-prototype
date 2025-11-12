73c66c6ceb06498d013ab592c8158fb1e41bc25e
sushitoad <36284902+sushitoad@users.noreply.github.com>
2025-11-12 11:33:48 -0800

    still refactoring charge bar

--- scripts/charge_bar.gd
+++ scripts/charge_bar.gd
@@ -12,33 +12,39 @@ var currentCharge: float = 0
 func _ready() -> void:
 	max_value = timeToCharge
 	value = 0
-	chargeTimer.wait_time = timeToCharge
-	chargeTimer.start()
 
 func _process(delta: float) -> void:
 	#value = timeToCharge - chargeTimer.time_left
+	
+	#this breaks when you reach max charges, I think it keeps giving you a 5th again immediately
 	if chargeTowerBoost < 1:
 		chargeTowerBoost = 1
-	currentCharge += delta * chargeTowerBoost
+	if currentCharge >= max_value:
+		if numberOfCharges < chargeCap:
+			currentCharge = 0
+			AddCharges(1)
+	else:
+		currentCharge += delta * chargeTowerBoost
 	value = currentCharge
 
 func _on_charge_timer_timeout() -> void:
-	AddCharges(1)
+	pass #AddCharges(1)
 
 func AddCharges(chargeAmount: int):
 	var chargesBefore: int = numberOfCharges
 	numberOfCharges += chargeAmount
 	if numberOfCharges == chargeCap:
-		chargeTimer.stop()
-		value = max_value
+		#chargeTimer.stop()
+		currentCharge = max_value
 	if numberOfCharges < 0:
 		numberOfCharges = 0
 	elif numberOfCharges > chargeCap:
 		numberOfCharges = chargeCap
-	if numberOfCharges < chargeCap and chargeTimer.is_stopped():
-		chargeTimer.start()
+	#if numberOfCharges < chargeCap and chargeTimer.is_stopped():
+	#	chargeTimer.start()
 	if chargesBefore != numberOfCharges:
 		chargeAmountChanged.emit()
+	print(str(numberOfCharges) + " charges")
 
 func _on_basic_tower_button_pressed() -> void:
 	AddCharges(-1)
