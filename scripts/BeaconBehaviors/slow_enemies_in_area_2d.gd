ab10ae2f68bba8afd753c6a5b5ec76ae4645413d
sushitoad <36284902+sushitoad@users.noreply.github.com>
2026-02-17 10:19:54 -0800

    more slow stuff

--- scripts/BeaconBehaviors/slow_enemies_in_area_2d.gd
+++ scripts/BeaconBehaviors/slow_enemies_in_area_2d.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var active: bool = false
 @export var defaultEnemySlowAmount: float
+@export var slowBonusForNearbyBeacon: float
+var currentSlowBonus: float
 var enemiesInRangeToSlow: Array[CharacterBody2D]
 var slowedEnemies: Array[CharacterBody2D]
 @onready var beacon: AnimatableBody2D = get_parent()
@@ -9,14 +11,15 @@ var slowedEnemies: Array[CharacterBody2D]
 func _ready() -> void:
 	beacon.on_destroyed.connect(UnslowAllEnemies)
 	beacon.on_placed.connect(Activate)
+	#connect adding and removing nearby beacons to func in this script
 
 func SlowEnemy(enemy: CharacterBody2D):
 	slowedEnemies.append(enemy)
-	enemy.speedReduction += defaultEnemySlowAmount
+	enemy.speedReduction += defaultEnemySlowAmount + currentSlowBonus
 
 func UnslowEnemy(enemy: CharacterBody2D):
 	slowedEnemies.remove_at(slowedEnemies.find(enemy))
-	enemy.speedReduction -= defaultEnemySlowAmount
+	enemy.speedReduction -= defaultEnemySlowAmount + currentSlowBonus
 
 func UnslowAllEnemies():
 	for enemy in slowedEnemies:
@@ -26,6 +29,10 @@ func Activate():
 	active = true
 	for enemy in enemiesInRangeToSlow:
 		SlowEnemy(enemy)
+
+func AddRemoveNearbyBeacon():
+	var numberOfNearby: int = beacon.nearbyBeacons.size()
+	currentSlowBonus += slowBonusForNearbyBeacon * numberOfNearby
 
 func _on_body_entered(body: Node2D) -> void:
 		if body.is_in_group("enemy"):
