8ab1a89384a3398cfb2da98610581eb74b3dd9b5
quillenink <36284902+sushitoad@users.noreply.github.com>
2026-02-23 23:08:28 -0800

    moving updates to sun beacon

--- scripts/sun_beacon.gd
+++ scripts/sun_beacon.gd
@@ -1,24 +1,45 @@
 extends Area2D
 
+var active: bool = false
 @export var defaultEnemySlowAmount: float = 20
-var totalEnemySlowAmount: float
-@export var nearbyBeaconSlowRangeBonus: float = 10
-@onready var beacon: StaticBody2D = get_parent()
-@onready var circleShape: Shape2D = $RangeShape2D.shape
-@onready var circleStartingRadius: float = circleShape.radius
+@export var slowBonusForNearbyBeacon: float = 10
+var currentSlowBonus: float = 0
 var enemiesInRangeToSlow: Array[CharacterBody2D]
 var slowedEnemies: Array[CharacterBody2D]
-var nearbyBeacons: Array[StaticBody2D]
+@onready var beacon: AnimatableBody2D = get_parent()
 
 func _ready() -> void:
-	beacon.on_destroyed.connect(StopSlowOnDormant)
-	beacon.on_awoke.connect(StartSlowOnAwoke)
-	totalEnemySlowAmount = defaultEnemySlowAmount
+	beacon.on_destroyed.connect(UnslowAllEnemies)
+	beacon.on_awoke.connect(Activate)
+	beacon.update_nearby_beacons.connect(UpdateNearbySlowBonus)
+
+
+func SlowEnemy(enemy: CharacterBody2D):
+	slowedEnemies.append(enemy)
+	enemy.speedReduction += defaultEnemySlowAmount + currentSlowBonus
+
+func UnslowEnemy(enemy: CharacterBody2D):
+	slowedEnemies.remove_at(slowedEnemies.find(enemy))
+	enemy.speedReduction -= defaultEnemySlowAmount + currentSlowBonus
+
+func Activate():
+	active = true
+	for enemy in enemiesInRangeToSlow:
+		SlowEnemy(enemy)
+
+func UnslowAllEnemies():
+	for enemy in slowedEnemies:
+		UnslowEnemy(enemy)
+
+func UpdateNearbySlowBonus():
+	var numberOfNearby: int = beacon.nearbyBeacons.size()
+	currentSlowBonus += slowBonusForNearbyBeacon * numberOfNearby
+	print(currentSlowBonus)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		enemiesInRangeToSlow.append(body)
-		if !beacon.isDestroyed and !beacon.isBeingPlaced:
+		if active:
 			SlowEnemy(body)
 
 func _on_body_exited(body: Node2D) -> void:
@@ -26,56 +47,3 @@ func _on_body_exited(body: Node2D) -> void:
 		enemiesInRangeToSlow.remove_at(enemiesInRangeToSlow.find(body))
 		if slowedEnemies.find(body) != -1:
 			UnslowEnemy(body)
-
-func StopSlowOnDormant():
-	SetSlowRange()
-	for enemy in slowedEnemies:
-		UnslowEnemy(enemy)
-
-func StartSlowOnAwoke():
-	SetSlowRange()
-	for enemy in enemiesInRangeToSlow:
-		if slowedEnemies.find(enemy) == -1:
-			SlowEnemy(enemy)
-
-func SlowEnemy(enemy: CharacterBody2D):
-	slowedEnemies.append(enemy)
-	enemy.speedReduction += defaultEnemySlowAmount
-
-func UnslowEnemy(enemy: CharacterBody2D):
-	slowedEnemies.remove_at(slowedEnemies.find(enemy))
-	enemy.speedReduction -= defaultEnemySlowAmount
-
-#why does this not happen when beacon is placed?
-#can I comb through this and make sure the basic behaviors are happening as intended?
-func SetSlowRange():
-	var uniqueBeacons: Array[StaticBody2D]
-	var otherSunBeaconCounter: int = 0
-	#print(nearbyBeacons)
-	for beacon in nearbyBeacons:
-		var type: GlobalEnums.BeaconType = beacon.beaconType
-		var isUnique: bool = false
-		if type == GlobalEnums.BeaconType.SUN:
-			otherSunBeaconCounter += 1
-		elif type != GlobalEnums.BeaconType.SUN:
-			isUnique = true
-			for unit in uniqueBeacons:
-				if type == unit.beaconType:
-					isUnique = false
-		if isUnique:
-			uniqueBeacons.append(beacon)
-	#print(uniqueBeacons)
-	#print(otherSunBeaconCounter)
-	circleShape.radius = circleStartingRadius + (uniqueBeacons.size() * nearbyBeaconSlowRangeBonus)
-	circleShape.radius = circleShape.radius - (otherSunBeaconCounter * nearbyBeaconSlowRangeBonus)
-	#print(circleShape.radius)
-	#im not updating the rangesprite and I honestly have no idea how I'd do that
-
-#this isn't running at all after the beacon is placed... whyyyyy?!
-func _on_nearby_beacons_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("beacon"):
-		print("nearby beacon: " + str(body.name))
-
-func _on_nearby_beacons_area_2d_body_exited(body: Node2D) -> void:
-	if body.is_in_group("beacon"):
-		print("bye bye " + str(body.name))
